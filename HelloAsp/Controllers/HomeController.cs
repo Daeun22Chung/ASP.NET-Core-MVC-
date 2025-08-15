484ffe33612f924ce50155f74bb8a22ebb7e7d19
ddaeu <ddaeu@DAEUN>
2025-08-16 00:17:13 +0900

    Add project files.

---
+++ HelloAsp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloAsp.Controllers
+{
+    public class HomeController : Controller 
+        // controller 에서의 핵심 요소는 Action을 정의하는 것 
+    {
+        public string Index() // 한개의 Action을 정의
+        {
+            return "응답입니다."; 
+        }
+    }
+}
+
+
+
+// routing system  (사용자의 요청 경로를 처리하는 시스템) 에서 요청경로와 Action 을 매핑(연결) 하는 과정 필요
+// Program.cs 에서 routing system 을 설정함
---
+++ HelloAsp/Program.cs
@@ -0,0 +1,16 @@
+﻿// 사용자의 사이트에 요청하는 url(요청경로)와 controller이 매핑되도록 설정해줌
+
+var builder = WebApplication.CreateBuilder(args); // 기본 윕 애플리케이션 생성은 WebApplicationBuilder 클래스를 사용하여 생성함  
+
+builder.Services.AddControllers(); // 컨트롤러를 사용하기 위해 서비스에 추가함. 
+
+var app = builder.Build(); // 요청처리, 경로설정, 서비스 추가 등에 사용
+
+//app.MapGet("/", () => "Hello World!");
+//app.MapControllers(); // 컨트롤러를 사용하기 위해 경로 설정함.
+app.MapControllerRoute( //MapControllerRoute 메서드는 사용자의 요청을 받는 새로운 endpoint 
+    name: "default", 
+    pattern: "{controller=Home}/{action=Index}/{id?}" 
+    );
+
+app.Run(); //시작
